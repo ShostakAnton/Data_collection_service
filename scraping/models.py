533793b190747b7129f652d730e4c29d34c2be00
Anton Shostak <60942970+ShostakAnton@users.noreply.github.com>
2020-07-03 00:46:44 +0300

    add models(City, Language, Vacancy)

---
+++ scraping/models.py
@@ -0,0 +1,65 @@
+from django.db import models
+
+from scraping.utils import from_cyrillic_to_eng
+
+
+class City(models.Model):
+    name = models.CharField(max_length=50,
+                            verbose_name='Название населенного пункта',
+                            unique=True)  # для уникальности данного поля
+    slug = models.CharField(max_length=50,
+                            blank=True,  # поле может быть пустым
+                            unique=True)
+
+    class Meta:
+        verbose_name = 'Название населенного пункта'
+        verbose_name_plural = 'Название населенных пунктов'
+
+    def __str__(self):
+        return self.name
+
+    def save(self, *args, **kwargs):  # переопределение метода save
+        if not self.slug:
+            self.slug = from_cyrillic_to_eng(str(self.name))
+            # super() - ф-ция которая определена в базовам классе моделей
+        super().save(*args, **kwargs)  # сохранение
+
+
+class Language(models.Model):
+    name = models.CharField(max_length=50,
+                            verbose_name='Язык программирования',
+                            unique=True)
+    slug = models.CharField(max_length=50, blank=True, unique=True)
+
+    class Meta:
+        verbose_name = 'Язык программирования'
+        verbose_name_plural = 'Языки программирования'
+
+    def __str__(self):
+        return self.name
+
+    def save(self, *args, **kwargs):  # переопределение метода save
+        if not self.slug:
+            self.slug = from_cyrillic_to_eng(str(self.name))
+            # super() - ф-ция которая определена в базовам классе моделей
+        super().save(*args, **kwargs)  # сохранение
+
+
+class Vacancy(models.Model):
+    url = models.URLField(unique=True)
+    title = models.CharField(max_length=250, verbose_name='Заголовок вакансии')
+    company = models.CharField(max_length=250, verbose_name='Компания')
+    description = models.TextField(verbose_name='Описание вакансии')
+    city = models.ForeignKey('City', on_delete=models.CASCADE,
+                             verbose_name='Город', related_name='vacancies')
+    language = models.ForeignKey('Language', on_delete=models.CASCADE,
+                                 verbose_name='Язык программирования')
+    timestamp = models.DateField(auto_now_add=True)
+
+    class Meta:
+        verbose_name = 'Вакансия'
+        verbose_name_plural = 'Вакансии'
+        ordering = ['-timestamp']
+
+    def __str__(self):
+        return self.title
